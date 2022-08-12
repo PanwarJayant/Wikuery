34afedaee9a51c66bfd5c8ea9685556baf9bb0d1
PanwarJayant <jayant.panwar@research.iiit.ac.in>
2022-08-12 20:47:47 +0530

    Added support for processing Infobox

--- wikiProcessor.py
+++ wikiProcessor.py
@@ -1,4 +1,30 @@
 from textProcessor import processText
 
-print(processText(
-    "https://a is the was am are programming beautifully$#@ goated game yesterday"))
+
+def processTitle(title):
+    processed_title = processText(title)
+    return processed_title
+
+
+def processInfobox(text):
+    processed_infobox = []
+    text = text.split('\n')
+    start = "{{Infobox"
+    end = "}}"
+    start_idx = 0
+    for str in text:
+        if(start in str):
+            break
+        start_idx += 1
+    text_data = []
+    text_data.append(' ')
+    start_idx += 1
+    end_idx = len(text)
+    for ch in range(start_idx, end_idx):
+        if(text[ch] == end):
+            break
+        text_data.append(text[ch])
+
+    infobox_data = ' '.join(text_data)
+    processed_infobox = processText(infobox_data)
+    return processed_infobox
